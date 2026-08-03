d69e8c4021d27dcce791e7af2cd7e375dc468393
workITtame <workITtame@users.noreply.github.com>
2026-08-03 18:01:36 +0300

    fix(api): move SchoolTable import to top of get_teachers function scope to resolve UnboundLocalError

--- routes/api_routes.py
+++ routes/api_routes.py
@@ -204,11 +204,11 @@ def get_teachers():
     class_id = request.args.get('class_id', type=int)
     
     try:
+        from models import SchoolTable
+        from models.academic import ClassSubject, TeacherSubject
         query = Teacher.query.filter_by(is_deleted=False)
         
         if class_id:
-            from models import SchoolTable
-            from models.academic import ClassSubject, TeacherSubject
             t_ids = [t[0] for t in db.session.query(SchoolTable.TeacherID).filter(SchoolTable.CID == class_id, SchoolTable.is_deleted == False).distinct().all() if t[0]]
             s_ids = [s[0] for s in db.session.query(ClassSubject.SubjectID).filter(ClassSubject.ClassID == class_id).all() if s[0]]
             if s_ids:
